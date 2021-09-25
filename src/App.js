99f9eac6d26f3b2c456640c574fa54949b637ffb
Rohan Singh <rohannsingh98765@gmail.com>
2021-09-26 02:01:55 +0530

    intialisation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
  import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
